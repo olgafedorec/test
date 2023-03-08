476a51415c35bb5243105d9517a9acec5ce757c6
olgafedorec <olga_fedorec@yahoo.com>
2023-03-08 19:18:06 +0200

    create js

---
+++ js/lesson01.js
@@ -0,0 +1,7 @@
+const message = prompt("Яка офіційна назва JavaScript?");
+if(message === "ECMAScript") {
+    alert("Вірно!");
+} else {
+    alert("Не знаєте? ECMAScript!");
+}
+console.log(message);
