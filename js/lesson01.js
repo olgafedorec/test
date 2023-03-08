ef5048ff31404b555191b1b917f5110bddaa0d8c
olgafedorec <olga_fedorec@yahoo.com>
2023-03-08 19:37:14 +0200

    Update lesson01.js

--- js/lesson01.js
+++ js/lesson01.js
@@ -1,7 +1,27 @@
-const message = prompt("Яка офіційна назва JavaScript?");
-if(message === "ECMAScript") {
-    alert("Вірно!");
-} else {
-    alert("Не знаєте? ECMAScript!");
-}
-console.log(message);
+// !---------------------------task01--------------------------- //
+
+// const message = prompt("Яка офіційна назва JavaScript?");
+// if(message === "ECMAScript") {
+//     alert("Вірно!");
+// } else {
+//     alert("Не знаєте? ECMAScript!");
+// }
+// console.log(message);
+
+// !---------------------------task02----------------------------//
+
+//Напишіть программу, яка отримує від користувача
+//число (кількість хвилин) и виводить у консоль
+//рядок у форматі годин та хвилин
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+const time = prompt("Введіть число");
+
+const hours = Number(time) / 60;
+
+console.log(hours);
+
+
+
+
