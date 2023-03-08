8f5bae30363f94971463bd604bdff638b5cc7ed9
olgafedorec <olga_fedorec@yahoo.com>
2023-03-08 20:24:22 +0200

    Update lesson01.js

--- js/lesson01.js
+++ js/lesson01.js
@@ -16,16 +16,42 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-const time = prompt("Введіть число");
+// const time = prompt("Введіть число");
 
-const hours = Math.floor(Number(time) / 60);
-const updateHours = String(hours).padStart(2, "0");
+// const hours = Math.floor(Number(time) / 60);
+// const updateHours = String(hours).padStart(2, "0");
 
-const minutes = time % 60;
-const updateMinutes = String(minutes).padStart(2, "0");
+// const minutes = time % 60;
+// const updateMinutes = String(minutes).padStart(2, "0");
 
-console.log(`${updateHours}:${updateMinutes}`);
+// console.log(`${updateHours}:${updateMinutes}`);
 
+// !-----------------------------task03-----------------------------//
 
+const null = 1;
+
+let result = "";
+
+switch (null) {
+    case 1:
+        result = "Зима";
+        break;
+
+    case 2:
+        result = "Весна";
+        break;
+    case 3:
+        result = "Літо";
+        break;
+    case 4:
+        result = "Осінь";
+        break;
+    
+
+    default:
+        result = "Введіть число від 1 до 4";
+}
+
+console.log(result);
 
 
